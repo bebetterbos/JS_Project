f65998942993308a1a962934061c3b1437a97b60
bebetterbos <bos34153@gmail.com>
2022-04-29 01:43:41 +0700

    commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const chalk = require('chalk');
 const app = express();
 const port = 3000;
 
@@ -7,5 +8,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log("Listening on port %d", port);
+    console.log("Listening on port", chalk.green(port));
 })
