f3dbf139e2c6b2aafeb1a7d9f1eebed330b1c813
bebetterbos <bos34153@gmail.com>
2022-04-29 21:32:17 +0700

    commit

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
+
+app.set("view", "./src/views");
+app.set("view engine", "ejs")
+
 
 app.get("/", (req, res) => {
     res.send('index', { username: 'Tanakrit', customer: ["Paramee,Kanokporn,Phumsan"] });
