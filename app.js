ccee937e558bf4be5ca8d7268826ace2b6ddd8a5
bebetterbos <bos34153@gmail.com>
2022-04-29 21:28:53 +0700

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(morgan('combined'));
 
 app.get("/", (req, res) => {
-    res.send('Hello World');
+    res.send('index', { username: 'Tanakrit', customer: ["Paramee,Kanokporn,Phumsan"] });
 })
 
 app.listen(port, () => {
